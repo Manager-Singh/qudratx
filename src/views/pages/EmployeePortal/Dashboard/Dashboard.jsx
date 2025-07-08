96bf8b2e0a048f75464b9d1ed7c37eb078439364
Ravinder Raj <ravinder.webtrack@gmail.com>
2025-07-08 15:29:07 +0530

    Employee dashboard

--- src/views/pages/EmployeePortal/Dashboard/Dashboard.jsx
+++ src/views/pages/EmployeePortal/Dashboard/Dashboard.jsx
@@ -1,17 +1,31 @@
 import React from "react";
-import Card from "../Card/Card";
+import Card from "../Components/Card/Card";
 
 const Dashboard =()=> {
     return (
-        <>
-        <Card
-        title="Welcome to Our Platform"
-        paragraph="Discover services tailored to your needs with expert guidance and support."
-        backgroundImage="https://via.placeholder.com/600x300"
-        textAlign="center"
-        />
+     <div className="container">
+        {/* section 1 */}
+        <div className="d-flex justify-content-around mb-5">
+             {/* Welcome card */}
+            <Card
+                title="Welcome Employee"
+                paragraph="Discover services tailored to your needs with expert guidance and support."
+                // backgroundImage="https://via.placeholder.com/600x300"
+                textAlign="center"
+            />
 
-        </>
+            {/* Notes card */}
+            <Card
+                title="Notes"
+                paragraph="Discover services tailored to your needs withDiscover services tailored to your needs withDiscover services tailored to your needs withDiscover services tailored to your needs withDiscover services tailored to your needs with expert guidance and support."
+                // backgroundImage="https://via.placeholder.com/600x300"
+                textAlign="center"
+            />
+        </div>
+       
+        {/* Section 2 */}
+        
+     </div>
     )
 }
 

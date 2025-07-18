b939a41b5c15438b47557f1fa1218926a44b08f4
manager singh <managersingh1991@gmail.com>
2025-07-18 16:08:16 +0530

    error resolved

--- server/controllers/proposalController.js
+++ server/controllers/proposalController.js
@@ -70,253 +70,253 @@ const createProposal = async (req, res) => {
 };
 
 // READ ALL
-const getProposal = async (req, res) => {
-  try {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
-    const offset = (page - 1) * limit;
-    const search = req.query.search || '';
-    const status = req.query.status; // optional: 'active' or 'inactive'
+// const getProposal = async (req, res) => {
+//   try {
+//     const page = parseInt(req.query.page) || 1;
+//     const limit = parseInt(req.query.limit) || 10;
+//     const offset = (page - 1) * limit;
+//     const search = req.query.search || '';
+//     const status = req.query.status; // optional: 'active' or 'inactive'
 
-    const where = {
-      deleted_at: null,
-      name: { [Op.like]: `%${search}%` }
-    };
-// filter by status if provided
-    if (status === 'active') {
-      where.status = true;
-    } else if (status === 'inactive') {
-      where.status = false;
-    }
-    const { count, rows } = await Proposal.findAndCountAll({
-      where,
-      limit,
-      offset,
-      order: [['created_at', 'DESC']],
-      include: [
-        {
-          model: BusinessZonesAuthority,
-          as: 'authority',
-          include: [
-            {
-              model: BusinessZone,
-              as: 'zone',
-            }
-          ]
-        }
-      ]
-    });
+//     const where = {
+//       deleted_at: null,
+//       name: { [Op.like]: `%${search}%` }
+//     };
+// // filter by status if provided
+//     if (status === 'active') {
+//       where.status = true;
+//     } else if (status === 'inactive') {
+//       where.status = false;
+//     }
+//     const { count, rows } = await Proposal.findAndCountAll({
+//       where,
+//       limit,
+//       offset,
+//       order: [['created_at', 'DESC']],
+//       include: [
+//         {
+//           model: BusinessZonesAuthority,
+//           as: 'authority',
+//           include: [
+//             {
+//               model: BusinessZone,
+//               as: 'zone',
+//             }
+//           ]
+//         }
+//       ]
+//     });
 
-    const totalPages = Math.ceil(count / limit);
+//     const totalPages = Math.ceil(count / limit);
 
-    res.status(200).json({
-      message: 'proposals fetched successfully',
-      page,
-      limit,
-      totalPages,
-      totalRecords: count,
-      data: rows
-    });
-  } catch (error) {
-    console.error('Get proposals error:', error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-};
+//     res.status(200).json({
+//       message: 'proposals fetched successfully',
+//       page,
+//       limit,
+//       totalPages,
+//       totalRecords: count,
+//       data: rows
+//     });
+//   } catch (error) {
+//     console.error('Get proposals error:', error);
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
+// };
 
-const getProposalByAuthorityId = async (req, res) => {
-  try {
-    const { authority_id } = req.params;
+// const getProposalByAuthorityId = async (req, res) => {
+//   try {
+//     const { authority_id } = req.params;
 
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
-    const offset = (page - 1) * limit;
-    const search = req.query.search || '';
-    const status = req.query.status;
+//     const page = parseInt(req.query.page) || 1;
+//     const limit = parseInt(req.query.limit) || 10;
+//     const offset = (page - 1) * limit;
+//     const search = req.query.search || '';
+//     const status = req.query.status;
 
-    const where = {
-      deleted_at: null,
-      authority_id,
-      name: { [Op.like]: `%${search}%` }
-    };
+//     const where = {
+//       deleted_at: null,
+//       authority_id,
+//       name: { [Op.like]: `%${search}%` }
+//     };
 
-    if (status === 'active') {
-      where.status = true;
-    } else if (status === 'inactive') {
-      where.status = false;
-    }
+//     if (status === 'active') {
+//       where.status = true;
+//     } else if (status === 'inactive') {
+//       where.status = false;
+//     }
 
-    const { count, rows } = await Proposal.findAndCountAll({
-      where,
-      limit,
-      offset,
-      order: [['created_at', 'DESC']],
-      include: [
-        {
-          model: BusinessZonesAuthority,
-          as: 'authority',
-          include: [
-            {
-              model: BusinessZone,
-              as: 'zone',
-            }
-          ]
-        }
-      ]
-    });
+//     const { count, rows } = await Proposal.findAndCountAll({
+//       where,
+//       limit,
+//       offset,
+//       order: [['created_at', 'DESC']],
+//       include: [
+//         {
+//           model: BusinessZonesAuthority,
+//           as: 'authority',
+//           include: [
+//             {
+//               model: BusinessZone,
+//               as: 'zone',
+//             }
+//           ]
+//         }
+//       ]
+//     });
 
-    const totalPages = Math.ceil(count / limit);
+//     const totalPages = Math.ceil(count / limit);
 
-    res.status(200).json({
-      message: 'Proposals fetched successfully by authority ID',
-      page,
-      limit,
-      totalPages,
-      totalRecords: count,
-      data: rows
-    });
-  } catch (error) {
-    console.error('Get Proposals by authority error:', error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-};
+//     res.status(200).json({
+//       message: 'Proposals fetched successfully by authority ID',
+//       page,
+//       limit,
+//       totalPages,
+//       totalRecords: count,
+//       data: rows
+//     });
+//   } catch (error) {
+//     console.error('Get Proposals by authority error:', error);
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
+// };
 
 
-// GET ONE
-const getProposalByUUID = async (req, res) => {
-  try {
-    const { uuid } = req.params;
+// // GET ONE
+// const getProposalByUUID = async (req, res) => {
+//   try {
+//     const { uuid } = req.params;
 
-    const package = await Proposal.findOne({ where: { uuid },include: [
-        {
-          model: BusinessZonesAuthority,
-          as: 'authority',
-          include: [
-            {
-              model: BusinessZone,
-              as: 'zone',
-            }
-          ]
-        }
-      ] });
-    if (!package) {
-      return res.status(404).json({ message: 'fee not found' });
-    }
+//     const package = await Proposal.findOne({ where: { uuid },include: [
+//         {
+//           model: BusinessZonesAuthority,
+//           as: 'authority',
+//           include: [
+//             {
+//               model: BusinessZone,
+//               as: 'zone',
+//             }
+//           ]
+//         }
+//       ] });
+//     if (!package) {
+//       return res.status(404).json({ message: 'fee not found' });
+//     }
 
-    res.status(200).json({
-      message: 'Proposal fetched successfully',
-      success: true,
-      data: package,
-    });
-  } catch (error) {
-    console.error('Get Proposal error:', error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-};
+//     res.status(200).json({
+//       message: 'Proposal fetched successfully',
+//       success: true,
+//       data: package,
+//     });
+//   } catch (error) {
+//     console.error('Get Proposal error:', error);
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
+// };
 
-// UPDATE
-const updateProposal = async (req, res) => {
-  try {
-    const { uuid } = req.params;
-     const { name,description, total_amount, status, subtotal,discount,fee_structure, tax, authority_id, activity } = req.body;
+// // UPDATE
+// const updateProposal = async (req, res) => {
+//   try {
+//     const { uuid } = req.params;
+//      const { name,description, total_amount, status, subtotal,discount,fee_structure, tax, authority_id, activity } = req.body;
 
-    if (!name || !total_amount || !subtotal || !fee_structure || !authority_id || !activity) {
-      return res.status(400).json({ message: 'Name, Fee structure, Subtotal, Authority Id, Activity and Total Amount are required' });
-    }
+//     if (!name || !total_amount || !subtotal || !fee_structure || !authority_id || !activity) {
+//       return res.status(400).json({ message: 'Name, Fee structure, Subtotal, Authority Id, Activity and Total Amount are required' });
+//     }
 
-    const proposal = await Proposal.findOne({ where: { uuid } });
-    if (!package) {
-      return res.status(404).json({ message: 'Package not found' });
-    }
+//     const proposal = await Proposal.findOne({ where: { uuid } });
+//     if (!package) {
+//       return res.status(404).json({ message: 'Package not found' });
+//     }
 
-    // proposal.name = name;
-    // proposal.dscription = description;
-    // proposal.total_amount = total_amount;
-    // package.status = status;
-    // package.subtotal = subtotal;
-    // package.fee_structure = fee_structure;
-    // package.authority_id = authority_id;
-    // package.activity = activity;
-    // package.updated_by = req.user.id;
-    // package.updated_at = new Date();
-    // package.last_update = new Date();
+//     // proposal.name = name;
+//     // proposal.dscription = description;
+//     // proposal.total_amount = total_amount;
+//     // package.status = status;
+//     // package.subtotal = subtotal;
+//     // package.fee_structure = fee_structure;
+//     // package.authority_id = authority_id;
+//     // package.activity = activity;
+//     // package.updated_by = req.user.id;
+//     // package.updated_at = new Date();
+//     // package.last_update = new Date();
 
-    await proposal.save({ userId: req.user.id });
+//     await proposal.save({ userId: req.user.id });
 
-    res.status(200).json({
-      message: 'Proposal updated successfully',
-      success: true,
-      data: proposal
-    });
-  } catch (error) {
-    console.error('Update proposal error:', error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-};
+//     res.status(200).json({
+//       message: 'Proposal updated successfully',
+//       success: true,
+//       data: proposal
+//     });
+//   } catch (error) {
+//     console.error('Update proposal error:', error);
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
+// };
 
-// DELETE
-const deleteProposal = async (req, res) => {
-  try {
-    const { uuid } = req.params;
+// // DELETE
+// const deleteProposal = async (req, res) => {
+//   try {
+//     const { uuid } = req.params;
 
-    const proposal = await Proposal.findOne({ where: { uuid } });
-    if (!proposal) {
-      return res.status(404).json({ message: 'proposal not found' });
-    }
+//     const proposal = await Proposal.findOne({ where: { uuid } });
+//     if (!proposal) {
+//       return res.status(404).json({ message: 'proposal not found' });
+//     }
 
-    await proposal.destroy({ userId: req.user.id }); // Soft delete because `paranoid: true`
+//     await proposal.destroy({ userId: req.user.id }); // Soft delete because `paranoid: true`
 
-    res.status(200).json({
-      message: 'Proposal deleted successfully',
-      success: true,
-      data: { uuid: proposal.uuid, name: proposal.name },
-    });
-  } catch (error) {
-    console.error('Delete proposal error:', error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-};
+//     res.status(200).json({
+//       message: 'Proposal deleted successfully',
+//       success: true,
+//       data: { uuid: proposal.uuid, name: proposal.name },
+//     });
+//   } catch (error) {
+//     console.error('Delete proposal error:', error);
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
+// };
 
-// GET DELETED Package
-const getDeletedProposal = async (req, res) => {
-  try {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
-    const offset = (page - 1) * limit;
-    const search = req.query.search || '';
+// // GET DELETED Package
+// const getDeletedProposal = async (req, res) => {
+//   try {
+//     const page = parseInt(req.query.page) || 1;
+//     const limit = parseInt(req.query.limit) || 10;
+//     const offset = (page - 1) * limit;
+//     const search = req.query.search || '';
 
-    const { count, rows } = await Proposal.findAndCountAll({
-      where: {
-        deleted_at: { [Op.not]: null },
-        name: { [Op.like]: `%${search}%` },
-      },
-      paranoid: false,
-      limit,
-      offset,
-      order: [['deleted_at', 'DESC']],
-    });
+//     const { count, rows } = await Proposal.findAndCountAll({
+//       where: {
+//         deleted_at: { [Op.not]: null },
+//         name: { [Op.like]: `%${search}%` },
+//       },
+//       paranoid: false,
+//       limit,
+//       offset,
+//       order: [['deleted_at', 'DESC']],
+//     });
 
-    const totalPages = Math.ceil(count / limit);
+//     const totalPages = Math.ceil(count / limit);
 
-    res.status(200).json({
-      message: 'Deleted proposal fetched successfully',
-      page,
-      limit,
-      totalPages,
-      totalRecords: count,
-      data: rows,
-    });
-  } catch (error) {
-    console.error('Get deleted proposal error:', error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-};
+//     res.status(200).json({
+//       message: 'Deleted proposal fetched successfully',
+//       page,
+//       limit,
+//       totalPages,
+//       totalRecords: count,
+//       data: rows,
+//     });
+//   } catch (error) {
+//     console.error('Get deleted proposal error:', error);
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
+// };
 
 module.exports = {
   createProposal,
-  getProposal,
-  getProposalByUUID,
-  getProposalByAuthorityId,
-  updateProposal,
-  deleteProposal,
-  getDeletedProposal,
+//   getProposal,
+//   getProposalByUUID,
+//   getProposalByAuthorityId,
+//   updateProposal,
+//   deleteProposal,
+//   getDeletedProposal,
 };
--- server/routes/admin/proposal.js
+++ server/routes/admin/proposal.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const { createProposal,
-  getProposalDetail,
-  getProposalDetailByUUID,
-  updateProposalDetail,
-  deleteProposalDetail,
-  getDeletedProposalDetail} = require('../../controllers/proposalController');
+  // getProposalDetail,
+  // getProposalDetailByUUID,
+  // updateProposalDetail,
+  // deleteProposalDetail,
+  // getDeletedProposalDetail
+} = require('../../controllers/proposalController');
 
 router.post('/create-proposal',createProposal)
-router.get('/get-proposal-detail',getProposalDetail)
-router.get('/get-proposal-detail-by-uuid/:uuid',getProposalDetailByUUID)
-router.put('/update-proposal-detail/:uuid',updateProposalDetail)
-router.delete('/delete-proposal-detail/:uuid',deleteProposalDetail)
-router.get('/get-deleted-proposal-detail',getDeletedProposalDetail)
+// router.get('/get-proposal-detail',getProposalDetail)
+// router.get('/get-proposal-detail-by-uuid/:uuid',getProposalDetailByUUID)
+// router.put('/update-proposal-detail/:uuid',updateProposalDetail)
+// router.delete('/delete-proposal-detail/:uuid',deleteProposalDetail)
+// router.get('/get-deleted-proposal-detail',getDeletedProposalDetail)
 
 
 module.exports = router;
